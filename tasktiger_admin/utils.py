0b53f4cfce045f0b477da14033ad662fbd399e71
Justin Crown <justin.crown@spinifexgroup.com>
2017-11-10 11:58:26 -0800

    adding interface option for admin CLI

--- tasktiger_admin/utils.py
+++ tasktiger_admin/utils.py
@@ -5,16 +5,19 @@ import redis
 from tasktiger import TaskTiger
 from tasktiger_admin import TaskTigerView
 
+
 @click.command()
 @click.option('-h', '--host', help='Redis server hostname')
 @click.option('-p', '--port', help='Redis server port')
 @click.option('-a', '--password', help='Redis password')
 @click.option('-n', '--db', help='Redis database number')
 @click.option('-l', '--listen', help='Admin port to listen on')
-def run_admin(host, port, db, password, listen):
+@click.option('-i', '--interface', help='Admin interface to listen on',
+              default='127.0.0.1')
+def run_admin(host, port, db, password, listen, interface):
     conn = redis.Redis(host, int(port or 6379), int(db or 0), password)
     tiger = TaskTiger(setup_structlog=True, connection=conn)
     app = Flask(__name__)
     admin = Admin(app, url='/')
     admin.add_view(TaskTigerView(tiger, name='TaskTiger', endpoint='tasktiger'))
-    app.run(debug=True, port=int(listen or 5000))
+    app.run(debug=True, port=int(listen or 5000), host=interface)
